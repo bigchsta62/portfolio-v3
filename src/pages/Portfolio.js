3611f4eb921545f6926eac16e870c1d40024fd73
bigchsta62 <65703232+bigchsta62@users.noreply.github.com>
2020-10-24 15:20:06 -0700

    final touches

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,8 +1,14 @@
 import React, { useState } from "react";
+import Link from "react-router-dom/Link";
 import Carousel from "react-bootstrap/Carousel";
 import { projectData } from "../data_sources/data_sources";
-
 import "./style.css";
+import styled, { keyframes } from "styled-components";
+import BounceIn from "react-animations/lib/bounce-in";
+const BounceInAnimation = keyframes`${BounceIn}`;
+const BounceInDiv = styled.div`
+  animation: infinite 5s ${BounceInAnimation};
+`;
 
 export default function ControlledCarousel() {
   const [index, setIndex] = useState(0);
@@ -24,15 +30,17 @@ export default function ControlledCarousel() {
             <img className="img-fluid" src={data.image} alt="First slide" />
             <Carousel.Caption>
               <footer>
-                <a href={data.url} target="_blank">
-                  <button
-                    className="btn btn-secondary"
-                    href={data.url}
-                    target="_blank"
-                  >
-                    <h3>See Deployment</h3>
-                  </button>
-                </a>
+                <BounceInDiv>
+                  <Link href={data.url} target="_blank">
+                    <button
+                      className="btn btn-secondary"
+                      href={data.url}
+                      target="_blank"
+                    >
+                      <h3>See Deployment</h3>
+                    </button>
+                  </Link>
+                </BounceInDiv>
               </footer>
             </Carousel.Caption>
           </Carousel.Item>
