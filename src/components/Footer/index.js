075928771e8d0600a0cbdc12eba1210187a46168
bigchsta62 <65703232+bigchsta62@users.noreply.github.com>
2021-01-29 16:18:36 -0700

    changed tab header and logo

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -6,7 +6,7 @@ export default function Footer() {
   return (
     <footer class="fixed-bottom bg-dark">
       <div class="container-flex">
-        <span class="text-muted text-center ">Progressive Improvement</span>
+        <span class="text-muted text-center ">TravisMiller.tech</span>
       </div>
     </footer>
   );
